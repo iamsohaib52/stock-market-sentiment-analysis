bb256bcc04db14f0d71f2f6800eb649a9f5a6320
M. Sohaib <sp22-bcs-057@cuivehari.edu.pk>
2025-08-13 02:48:51 +0500

    Initial commit

---
+++ webapp/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import pickle
+import os
+
+MODEL_PATH = "model_tfidf.pkl"
+VECTORIZER_PATH = "vectorizer_tfidf.pkl"
+
+with open(MODEL_PATH, "rb") as f:
+    model = pickle.load(f)
+with open(VECTORIZER_PATH, "rb") as f:
+    vectorizer = pickle.load(f)
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    prediction = None
+    input_text = ""
+    if request.method == "POST":
+        input_text = request.form.get("text")
+        if input_text:
+            transformed_text = vectorizer.transform([input_text])
+            pred = model.predict(transformed_text)[0]
+            if pred in ["Positive", "Negative"]:
+                prediction = pred
+            else:
+                prediction = "Unknown"  # This should never happen unless model is broken
+
+    return render_template("index.html", prediction=prediction, input_text=input_text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
